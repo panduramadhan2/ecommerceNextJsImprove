b86138a35effbbaacb435df3b9c3b108eb7a2845
panduramadhan2 <panduramadhan2@gmail.com>
2024-07-05 16:48:19 +0700

    solving clerk

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,18 +1,11 @@
 import { Button } from "@/components/ui/button";
-import { SignIn, SignOutButton, useAuth } from "@clerk/nextjs";
+import { SignIn, SignOutButton, useAuth, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
-  // const { sessionId } = useAuth();
   return (
     <div className="p-4">
-      <Button size="lg" variant="destructive">
-        Click me
-      </Button>
-      {/* <SignOutButton signOutOptions={{ sessionId }}>
-        <Button>Sign out</Button>
-      </SignOutButton> */}
-      {/* <SignIn /> */}
+      <UserButton />
     </div>
   );
 }
