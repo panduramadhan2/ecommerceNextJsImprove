439027e85d9ab69c63a9326f70fd5f647c68feca
panduramadhan2 <panduramadhan2@gmail.com>
2024-07-08 16:29:09 +0700

    checkpoint Komponen Form Input (Admin)

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,41 @@
-import { Button } from "@/components/ui/button";
-import { SignIn, SignOutButton, useAuth, UserButton } from "@clerk/nextjs";
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import Modal from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { RedirectToSignIn, UserButton, useAuth } from "@clerk/nextjs";
+import { useEffect } from "react";
+// import { useAuth } from "@clerk/clerk-react";
+
+const SetupPage = () => {
+  const { isSignedIn } = useAuth();
+
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
+  if (!isSignedIn) {
+    return <RedirectToSignIn />;
+  }
+
   return (
     <div className="p-4">
-      <UserButton />
+      {/* <UserButton /> */}
+      {/* <Modal
+        title="Test Title"
+        description="test Description"
+        isOpen
+        onClose={() => {}}
+      >
+        Children
+      </Modal> */}
+      Root Page
     </div>
   );
-}
+};
+
+export default SetupPage;
--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,36 @@
+// import { clerkMiddleware } from "@clerk/nextjs/server";
+
+// export default clerkMiddleware();
+
+// export const config = {
+//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
+import { NextResponse } from "next/server";
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+// Define your public routes
+const publicRoutes = ["/api/:path*"];
+
+function isPublicRoute(pathname: string) {
+  return publicRoutes.some((route) => {
+    const regex = new RegExp(`^${route.replace(/:\w+(\*)?/g, ".*")}$`);
+    return regex.test(pathname);
+  });
+}
+
+const middleware = clerkMiddleware();
+
+export default function handler(req: any, ev: any) {
+  const { pathname } = req.nextUrl;
+
+  // If it's a public route, allow the request to proceed without Clerk's middleware
+  if (isPublicRoute(pathname)) {
+    return NextResponse.next();
+  }
+
+  // Otherwise, apply Clerk's middleware
+  return middleware(req, ev);
+}
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
