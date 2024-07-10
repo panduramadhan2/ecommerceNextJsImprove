27c9b1fe3b9736bbb341cc40fab2c39dd87a85b4
panduramadhan2 <panduramadhan2@gmail.com>
2024-07-10 15:58:36 +0700

    troubleshot aftersignout userbutton

---
+++ components/main-nav.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+export function MainNav({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLElement>) {
+  return <nav>This is Main Nav</nav>;
+}
---
+++ components/navbar.tsx
@@ -0,0 +1,18 @@
+import { UserButton } from "@clerk/nextjs";
+import { MainNav } from "./main-nav";
+
+const Navbar = () => {
+  return (
+    <div className="border-b">
+      <div className="flex h-16 items-center px-4">
+        <div>Store Switcher</div>
+        <MainNav />
+        <div className="ml-auto flex items-center space-x-4">
+          <UserButton />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
