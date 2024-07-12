8ee1395dac33e9dbff81f03db2bfddeab6f57aef
panduramadhan2 <panduramadhan2@gmail.com>
2024-07-12 11:04:05 +0700

    Checkpoint Hapus Toko Admin

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -57,12 +57,27 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
     }
   };
 
+  const onDelete = async () => {
+    try {
+      setLoading(true);
+      await axios.delete(`/api/stores/${params.storeId}`);
+      router.refresh();
+      router.push("/");
+      toast.success("Toko berhasil dihapus");
+    } catch (error) {
+      toast.error("Cek kembali data dan koneksi mu");
+    } finally {
+      setLoading(false);
+      setOpen(false);
+    }
+  };
+
   return (
     <>
       <AlertModal
         isOpen={open}
         onClose={() => setOpen(false)}
-        onConfirm={() => {}}
+        onConfirm={onDelete}
         loading={loading}
       />
       <div className="flex items-center justify-between">
