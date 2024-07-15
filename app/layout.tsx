4ded978a1a04c726cff62982ec71a582f0b09d96
panduramadhan2 <panduramadhan2@gmail.com>
2024-07-15 15:08:07 +0700

    fixing deploy and ready

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ import { ModalProvider } from "@/providers/modal-provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Ali Admin",
-  description: "Ali Admin",
+  title: "Pandu Admin",
+  description: "Pandu Admin",
 };
 
 export default function RootLayout({
